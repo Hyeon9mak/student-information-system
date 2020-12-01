675a42d8a76819bd401188a0d0bd3e7832fc7458
Hyeon9mak <jinha3507@gmail.com>
2020-12-01 17:32:39 +0900

    test: add students ArrayList test code

--- src/test/java/CourseSessionTest.java
+++ src/test/java/CourseSessionTest.java
@@ -13,9 +13,15 @@ public class CourseSessionTest extends junit.framework.TestCase {
         Student student1 = new Student("Hyeon Gu");
         session.enroll(student1);
         assertEquals(1, session.getNumberOfStudents());
+        java.util.ArrayList<Student> allStudents = session.getAllStudents();
+        assertEquals(1, allStudents.size());
+        assertEquals(student1, allStudents.get(0));
 
         Student student2 = new Student("Dong Hyeon");
         session.enroll(student2);
         assertEquals(2, session.getNumberOfStudents());
+        assertEquals(2, allStudents.size());
+        assertEquals(student1, allStudents.get(0));
+        assertEquals(student2, allStudents.get(1));
     }
 }
