b6578e8d72880c3ace9d9a725c9afac10427450d
Hyeon9mak <jinha3507@gmail.com>
2020-12-01 16:05:17 +0900

    test: add test suite code

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,8 @@
+public class AllTests {
+    public static junit.framework.TestSuite suite() {
+        junit.framework.TestSuite suite = new junit.framework.TestSuite();
+        suite.addTestSuite(StudentTest.class);
+        suite.addTestSuite(CourseSessionTest.class);
+        return suite;
+    }
+}
