2d6c83583ecb3e13ac3214b86db72d174bea0139
Hyeon9mak <jinha3507@gmail.com>
2020-12-01 18:33:20 +0900

    refactor: add setUp method
    add CourseSession class constructor method (setUp)

--- src/test/java/studentinfo/CourseSessionTest.java
+++ src/test/java/studentinfo/CourseSessionTest.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 
 public class CourseSessionTest extends TestCase {
 
+    private CourseSession session;
+
+    public void setUp() {
+        session = new CourseSession("ENGL", "101");
+    }
+
     public void testCreate() {
-        CourseSession session = new CourseSession("ENGL", "101");
         assertEquals("ENGL", session.getDepartment());
         assertEquals("101", session.getNumber());
         assertEquals(0, session.getNumberOfStudents());
     }
 
     public void testEnrolStudents() {
-        CourseSession session = new CourseSession("ENGL", "101");
-
         Student student1 = new Student("Hyeon Gu");
         session.enroll(student1);
         assertEquals(1, session.getNumberOfStudents());
