af0947227864343c00a553f4f002af1f2fc2cc51
Hyeon9mak <jinha3507@gmail.com>
2020-12-01 17:42:09 +0900

    feat: Student 객체 리스트 관리 기능 추가 Student 객체 리스트 students 추가 students 반환 메서드 추가 강의 수강 메서드(enroll)에 학생 객체가 추가되면 students에도 추가되도록 기능 수정

--- src/main/java/CourseSession.java
+++ src/main/java/CourseSession.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class CourseSession {
 
     private String department;
     private String number;
     private int numberOfStudents = 0;
+    private java.util.ArrayList<Student> students =
+        new java.util.ArrayList<Student>();
 
     public CourseSession(String department, String number) {
         this.department = department;
@@ -23,5 +27,10 @@ public class CourseSession {
 
     public void enroll(Student student) {
         numberOfStudents += 1;
+        students.add(student);
+    }
+
+    public ArrayList<Student> getAllStudents() {
+        return students;
     }
 }
