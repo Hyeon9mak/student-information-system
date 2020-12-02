72f65ba1b1e71f1f108f116937dcbd45f48af0e6
Hyeon9mak <jinha3507@gmail.com>
2020-12-02 10:53:12 +0900

    feat: add getEndDate, getStartDate methods

--- src/main/java/studentinfo/CourseSession.java
+++ src/main/java/studentinfo/CourseSession.java
@@ -1,16 +1,21 @@
 package studentinfo;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class CourseSession {
 
     private String department;
     private String number;
     private ArrayList<Student> students = new ArrayList<Student>();
+    private Date startDate;
 
-    public CourseSession(String department, String number) {
+    public CourseSession(String department, String number, Date startDate) {
         this.department = department;
         this.number = number;
+        this.startDate = startDate;
     }
 
     public String getDepartment() {
@@ -31,5 +36,18 @@ public class CourseSession {
 
     public Student get(int index) {
         return students.get(index);
+    }
+
+    public Date getEndDate() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(startDate);
+        int numberOfDays = 16 * 7 - 3;
+        calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+        Date endDate = calendar.getTime();
+        return endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
     }
 }
