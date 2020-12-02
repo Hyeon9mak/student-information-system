79d67df49e74143329064ca44346996599290591
Hyeon9mak <jinha3507@gmail.com>
2020-12-02 14:21:34 +0900

    test: delete color variable

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,9 +1,6 @@
 public class PawnTest extends junit.framework.TestCase {
 
     public void testCreate() {
-        final String firstPawnColor = "black";
-        final String secondPawnColor = "white";
-
         Pawn firstPawn = new Pawn(firstPawnColor);
         assertEquals(firstPawn.getColor(), firstPawnColor);
 
