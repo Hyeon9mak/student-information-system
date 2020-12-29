9380a056a9b8aa583149026fb9881da5bf0a5e20
Hyeon9mak <jinha3507@gmail.com>
2020-12-29 15:27:56 +0900

    fix: fix final keyword

--- src/main/java/studentinfo/RosterReporter.java
+++ src/main/java/studentinfo/RosterReporter.java
@@ -6,12 +6,12 @@ public class RosterReporter {
     static final String ROSTER_REPORT_HEADER = "Student" + NEWLINE + "-" + NEWLINE;
     static final String ROSTER_REPORT_FOOTER = NEWLINE + "# students = ";
 
-    private final CourseSession session;
+    private CourseSession session;
 
     RosterReporter(CourseSession session) {
         this.session = session;
     }
-    
+
     String getReport() {
         StringBuilder buffer = new StringBuilder();
 
