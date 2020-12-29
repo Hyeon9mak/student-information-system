5c6b97d5de8cfec9104ac171d4d36d7403a95ec5
Hyeon9mak <jinha3507@gmail.com>
2020-12-29 15:28:59 +0900

    style: whitespace and enterline

--- src/main/java/studentinfo/DateUtil.java
+++ src/main/java/studentinfo/DateUtil.java
@@ -10,7 +10,7 @@ public class DateUtil {
         GregorianCalendar calendar = new GregorianCalendar();
         calendar.clear();
         calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month-1);
+        calendar.set(Calendar.MONTH, month - 1);
         calendar.set(Calendar.DAY_OF_MONTH, date);
         return calendar.getTime();
     }
--- src/test/java/studentinfo/DateUtilTest.java
+++ src/test/java/studentinfo/DateUtilTest.java
@@ -1,7 +1,5 @@
 package studentinfo;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
