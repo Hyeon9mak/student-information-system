85681f24e2ba83c594282d2b7de2ea98e886a779
Hyeon9mak <jinha3507@gmail.com>
2020-12-31 14:34:49 +0900

    test: add test for Student is FullTime

--- src/test/java/sis/studentinfo/StudentTest.java
+++ src/test/java/sis/studentinfo/StudentTest.java
@@ -28,6 +28,24 @@ public class StudentTest extends TestCase {
         assertEquals(7, student.getCredits());
     }
 
+    public void testStudentStatus() {
+        Student student = new Student("a");
+        assertEquals(0, student.getCredits());
+        assertFalse(student.isFullTime());
+
+        student.addCredits(3);
+        assertEquals(3, student.getCredits());
+        assertFalse(student.isFullTime());
+
+        student.addCredits(4);
+        assertEquals(7, student.getCredits());
+        assertFalse(student.isFullTime());
+
+        student.addCredits(5);
+        assertEquals(12, student.getCredits());
+        assertTrue(student.isFullTime());
+    }
+
 //    public void testBadStatic() {
 //        Student studentA = new Student("a");
 //        assertEquals("a", studentA.getName());
