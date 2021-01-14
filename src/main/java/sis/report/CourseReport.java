cca4e1e256ec980148bacbae06ce03e9f1901e32
Hyeon9mak <jinha3507@gmail.com>
2021-01-14 14:21:47 +0900

    feat: create CourseReport class

--- src/main/java/sis/report/CourseReport.java
+++ src/main/java/sis/report/CourseReport.java
@@ -1,2 +1,23 @@
-package sis.report;public class CourseReport {
+package sis.report;
+
+import static sis.report.ReportConstant.NEWLINE;
+
+import java.util.ArrayList;
+import sis.studentinfo.CourseSession;
+
+public class CourseReport {
+
+    private final ArrayList<CourseSession> sessions = new ArrayList<CourseSession>();
+
+    public void add(CourseSession session) {
+        sessions.add(session);
+    }
+
+    public String text() {
+        StringBuilder builder = new StringBuilder();
+        for (CourseSession session : sessions) {
+            builder.append(session.getDepartment() + " " + session.getNumber() + NEWLINE);
+        }
+        return builder.toString();
+    }
 }
--- src/test/java/sis/report/CourseReportTest.java
+++ src/test/java/sis/report/CourseReportTest.java
@@ -1,15 +1,16 @@
 package sis.report;
 
+import static sis.report.ReportConstant.NEWLINE;
+
 import java.util.Date;
 import junit.framework.TestCase;
 import sis.studentinfo.CourseSession;
-import static sis.report.ReportConstant.NEWLINE;
 
 public class CourseReportTest extends TestCase {
 
     public void testReport() {
         final Date date = new Date();
-        CourseReport report = new CourseReportTest();
+        CourseReport report = new CourseReport();
         report.add(CourseSession.create("ENGL", "101", date));
         report.add(CourseSession.create("CZEC", "200", date));
         report.add(CourseSession.create("ITAL", "410", date));
