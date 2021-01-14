35e4459b1dc477beb1c5989872ec9efbee3f4240
Hyeon9mak <jinha3507@gmail.com>
2021-01-14 14:28:16 +0900

    test: add CourseReportTest suite in AllTests

--- src/test/java/sis/report/AllTests.java
+++ src/test/java/sis/report/AllTests.java
@@ -7,6 +7,7 @@ public class AllTests {
     public static TestSuite suite() {
         TestSuite suite = new TestSuite();
         suite.addTestSuite(RosterReporterTest.class);
+        suite.addTestSuite(CourseReportTest.class);
         return suite;
     }
 }
