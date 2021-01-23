bd4812c0212ba1e684200abd73727951a674543d
Hyeon9mak <jinha3507@gmail.com>
2021-01-23 16:48:08 +0900

    test: add Student Gpa test method

--- src/test/java/sis/studentinfo/StudentTest.java
+++ src/test/java/sis/studentinfo/StudentTest.java
@@ -4,6 +4,8 @@ import junit.framework.TestCase;
 
 public class StudentTest extends TestCase {
 
+    private static final double GRADE_TOLERANCE = 0.05;
+
     public void testCreate() {
         final String firstStudentName = "Hyeon Gu";
         Student student = new Student(firstStudentName);
@@ -53,6 +55,21 @@ public class StudentTest extends TestCase {
         assertTrue(student.isInState());
         student.setState("MD");
         assertFalse(student.isInState());
+    }
+
+    public void testCalculateGpa() {
+        Student student = new Student("a");
+        assertEquals(0.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("A");
+        assertEquals(4.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("B");
+        assertEquals(3.5, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("C");
+        assertEquals(3.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("D");
+        assertEquals(2.5, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("F");
+        assertEquals(2.0, student.getGpa(), GRADE_TOLERANCE);
     }
 
 //    public void testBadStatic() {
