f6d5afc9ae9804a680ad22ea2bec459ed7add5ee
Hyeon9mak <jinha3507@gmail.com>
2021-01-23 16:48:24 +0900

    feat: add Student Grade & Gpa methods

--- src/main/java/sis/studentinfo/Student.java
+++ src/main/java/sis/studentinfo/Student.java
@@ -1,11 +1,14 @@
 package sis.studentinfo;
 
+import java.util.ArrayList;
+
 public class Student {
 
     static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
     static final String IN_STATE = "CO";
 
     private final String name;
+    private final ArrayList<String> grades = new ArrayList<String>();
     private int credits;
     private String state = "";
 
@@ -36,5 +39,35 @@ public class Student {
 
     public void setState(String state) {
         this.state = state;
+    }
+
+    void addGrade(String grade) {
+        grades.add(grade);
+    }
+
+    public double getGpa() {
+        if (grades.isEmpty()) {
+            return 0.0;
+        }
+        double total = 0.0;
+        for (String grade : grades) {
+            if (grade.equals("A")) {
+                total += 4;
+            } else {
+                if (grade.equals("B")) {
+                    total += 3;
+                } else {
+                    if (grade.equals("C")) {
+                        total += 2;
+                    } else {
+                        if (grade.equals("D")) {
+                            total += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return total / grades.size();
     }
 }
